188c5a4f90da7d0d480e9447a788d5aac35d373b
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-05-07 17:39:20 -0400

    Remove more unused stuff

--- sockpuppet/settings.py
+++ sockpuppet/settings.py
@@ -9,7 +9,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SOCKPUPPET_SECRET', 'secret-key')  # TODO: Change me
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
-    BCRYPT_LOG_ROUNDS = 13
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
@@ -51,5 +50,4 @@ class TestConfig(Config):
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
-    BCRYPT_LOG_ROUNDS = 4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
     WTF_CSRF_ENABLED = False  # Allows form testing
