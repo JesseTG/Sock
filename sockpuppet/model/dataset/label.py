bfb5771f5d61362a036cdb620464ce4b58d14af3
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-07-29 13:44:47 -0400

    Add some Dataset subclasses

---
+++ sockpuppet/model/dataset/label.py
@@ -0,0 +1,33 @@
+from typing import Any, Sequence, Tuple, TypeVar
+
+from torch.utils.data.dataset import Dataset
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+class LabelDataset(Dataset):
+    def __init__(self, data: Sequence[T], labels: Sequence[U]):
+        self.data = data
+        self.labels = labels
+
+        if len(data) != len(labels):
+            raise ValueError(f"data and labels must have the same length ({len(data)} vs {len(labels)})")
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> Tuple[T, U]:
+        return (self.data[index], self.labels[index])
+
+
+class SingleLabelDataset(Dataset):
+    def __init__(self, data: Sequence[T], label: Any):
+        self.data = data
+        self.label = label
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> Tuple[T, Any]:
+        return (self.data[index], self.label)
