94c66ffd1d2cb69103b1512a6ba3c956b91ead8c
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-07-29 13:40:33 -0400

    Add TRAINING_DATA_PATH environment variable

--- sockpuppet/settings.py
+++ sockpuppet/settings.py
@@ -25,7 +25,7 @@ class Config(object):
     CACHE_REDIS_PORT = os.environ.get("SOCKPUPPET_REDIS_PORT", 6379)
     CACHE_REDIS_PASSWORD = os.environ.get("SOCKPUPPET_REDIS_PASSWORD")
     CACHE_REDIS_DB = os.environ.get("SOCKPUPPET_REDIS_DB", 0)
-    # TODO: Make this more robust
+    TRAINING_DATA_PATH = os.environ.get("SOCKPUPPET_TRAINING_DATA_PATH", "/home/jesse/data")
 
 
 class ProdConfig(Config):
