4313ca4ff44ae6162a98389605f464fca03841f9
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-13 19:53:38 -0400

    Add some new marks

--- tests/marks.py
+++ tests/marks.py
@@ -3,3 +3,5 @@ import torch
 
 needs_cuda = pytest.mark.skipif(not torch.cuda.is_available(), reason="This test requires CUDA")
 needs_cudnn = pytest.mark.skipif(not torch.backends.cudnn.is_available(), reason="This test requires CUDNN")
+cpu_only = pytest.mark.cpu_only
+cuda_only = pytest.mark.cuda_only
