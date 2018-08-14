574e746c2721d6ea34c6aaa453b23a078bd3836c
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-14 10:31:39 -0400

    Case-insensitive comparisons

--- sockpuppet/model/data/extract_bots.py
+++ sockpuppet/model/data/extract_bots.py
@@ -9,16 +9,18 @@ import sys
 def main():
     with open(sys.argv[1], "r") as users:
         # TODO: Give this via a command line arg
-        userset = frozenset(u.strip() for u in users.readlines())
+        userset = frozenset(u.casefold() for u in users.readlines())
         for line in fileinput.input("-"):
             # TODO: Generalize to multiple input streams
             tweet = json.loads(line)
 
             if "user" in tweet:
                 user = tweet["user"]
+
                 if "screen_name" in user:
                     screen_name = user["screen_name"]
-                    if screen_name in userset:
+
+                    if screen_name.casefold() in userset:
                     print(line, end="")
 
 if __name__ == "__main__":
