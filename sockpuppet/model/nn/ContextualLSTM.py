d41b54a35efba5f369c1f0f1e3f22b3cc9127e7e
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-14 10:58:38 -0400

    ContextualLSTM improvements
    - Support different devices explicitly
    - Reset hidden state between sequences
    - Reshape the output

--- sockpuppet/model/nn/ContextualLSTM.py
+++ sockpuppet/model/nn/ContextualLSTM.py
@@ -1,4 +1,4 @@
-from typing import Union, Sequence
+from typing import Union, Sequence, Tuple
 
 import torch
 from torch import nn
@@ -11,7 +11,12 @@ from sockpuppet.model.embedding import WordEmbeddings
 
 
 class ContextualLSTM(nn.Module):
-    def __init__(self, word_embeddings: WordEmbeddings, hidden_layers: int=32):
+    def __init__(
+        self,
+        word_embeddings: WordEmbeddings,
+        hidden_layers: int=32,
+        device: Union[torch.device, str]="cpu"
+    ):
         super(ContextualLSTM, self).__init__()
         self.word_embeddings = word_embeddings
 
@@ -21,20 +26,41 @@ class ContextualLSTM(nn.Module):
         self.lstm = nn.LSTM(word_embeddings.dim, hidden_layers, batch_first=False)
         self.dense1 = nn.Linear(hidden_layers, 128)
         self.dense2 = nn.Linear(self.dense1.out_features, 64)
-        self.output = nn.Linear(self.dense2.out_features, 2)
+        self.output = nn.Linear(self.dense2.out_features, 1)
 
+        self.to(device)
         # note: there are special data parallelism modules.  read about them!
         # is there a layer that takes the weighted average of two like-shaped tensors? would be useful
         # for mixing the main output and the aux output like the paper describes
         # if not, just mix them myself
 
+    @property
+    def device(self) -> torch.device:
+        return self.dense1.weight.device
+
+    def _init_hidden(self, batch_size) -> Tuple[Tensor, Tensor]:
+        def make_zeros():
+            return torch.zeros(
+                self.lstm.num_layers,
+                batch_size,
+                self.lstm.hidden_size,
+                dtype=torch.float,
+                device=self.device
+            )
+
+        return (make_zeros(), make_zeros())
+
+    def extra_repr(self) -> str:
+        return f"<device>: {self.device}"
+
     def forward(self, sentences) -> Tensor:
         # IN: List of LongTensors
         # OUT: One FloatTensor
+        self.hidden = self._init_hidden(len(sentences))
 
         # TODO: Reset hidden state before each batch
         sentences = sorted(sentences, key=len, reverse=True)
-        lengths = LongTensor([len(s) for s in sentences], device=sentences[0].device)
+        lengths = LongTensor([len(s) for s in sentences], device=self.device)
 
         padded = pad_sequence(sentences, False, self.embeddings.padding_idx)
         # ^ Size([num_tweets, longest_tweet])
@@ -43,7 +69,7 @@ class ContextualLSTM(nn.Module):
         # ^ Size([num_tweets, longest_tweet, self.word_embeddings.dim])
 
         packed = pack_padded_sequence(embedding, lengths, False)
-        out, (hn, cn) = self.lstm(packed)
+        out, (hn, cn) = self.lstm(packed, self.hidden)
         # ^ Size([num_tweets, num_tokens, num_dims]) -> Size([???])
         # TODO: Figure out exactly what the dimensions are
         # out: Output features on every element (word vector) of the input
@@ -55,7 +81,7 @@ class ContextualLSTM(nn.Module):
 
         a = functional.relu(self.dense1(hn))  # Size([???]) -> Size([???])
         b = functional.relu(self.dense2(a))  # Size([???]) -> Size([???])
-        c = torch.sigmoid(self.output(b))  # Size([???]) -> Size([num_tweets, 2])
-        return c
-        # TODO: May have to add another dimension for each of two classes (yes or no)
+        c = torch.sigmoid(self.output(b))  # Size([???]) -> Size([num_tweets, 1])
+        return c.view(len(sentences))
+        # TODO: Consider using BCEWithLogitsLoss
         # TODO: What optimizer did the paper use?  What loss function?
