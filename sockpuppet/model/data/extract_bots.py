d044e941b206e90683000c8d7e86ed685b7a12eb
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-14 10:43:13 -0400

    Oops, forgot to strip out extra whitespace

--- sockpuppet/model/data/extract_bots.py
+++ sockpuppet/model/data/extract_bots.py
@@ -9,7 +9,7 @@ import sys
 def main():
     with open(sys.argv[1], "r") as users:
         # TODO: Give this via a command line arg
-        userset = frozenset(u.casefold() for u in users.readlines())
+        userset = frozenset(u.casefold().strip() for u in users.readlines())
         for line in fileinput.input("-"):
             # TODO: Generalize to multiple input streams
             tweet = json.loads(line)
