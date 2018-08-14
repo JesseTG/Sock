b8b9de2604406700222613553ffb37c1399b4a28
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-14 13:11:35 -0400

    Change batching in sentence_collate_batch

--- sockpuppet/model/dataset/sentence_collate.py
+++ sockpuppet/model/dataset/sentence_collate.py
@@ -1,4 +1,5 @@
 from typing import Sequence, Union, Tuple
+import torch
 from torch import Tensor, LongTensor
 from torch.nn.utils.rnn import pad_sequence
 
@@ -13,8 +14,8 @@ def sentence_collate(sentences: Sequence[LongTensor]) -> LongTensor:
 def sentence_collate_batch(sentences: Sequence[Tuple[LongTensor, int]]) -> Tuple[LongTensor, Sequence[int]]:
     sentences = sorted(sentences, key=lambda s: len(s[0]), reverse=True)
     encodings = [s[0] for s in sentences]
-    labels = LongTensor([s[1] for s in sentences], device=sentences[0][0].device)
+    labels = torch.tensor([s[1] for s in sentences], dtype=torch.float, device=sentences[0][0].device)
 
-    padded = pad_sequence(encodings, False, 0)
+    padded = pad_sequence(encodings, True, 0)
 
     return padded, labels
