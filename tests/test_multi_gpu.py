148bf029e329f79476d133db04a130a8247a9a05
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-15 11:05:06 -0400

    Add test_multi_gpu.py

---
+++ tests/test_multi_gpu.py
@@ -0,0 +1,91 @@
+from typing import Callable, Sequence, Tuple
+from collections import namedtuple
+import time
+
+import pytest
+import torch
+import ignite
+
+from torch import Tensor, LongTensor
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.nn.utils.rnn import pad_sequence
+from torch.nn import DataParallel
+from ignite.engine import Events, Engine
+import ignite.metrics
+from sockpuppet.model.nn.ContextualLSTM import ContextualLSTM
+from sockpuppet.model.embedding import WordEmbeddings
+from sockpuppet.model.dataset import LabelDataset, sentence_collate_batch
+from tests.marks import needs_cuda, needs_cudnn, needs_multiple_gpus, cuda_only
+
+
+torch.manual_seed(0)
+
+
+@cuda_only
+@needs_cuda
+@needs_multiple_gpus
+def test_create_data_parallel_lstm(lstm: ContextualLSTM):
+    dataparallel = DataParallel(lstm)
+
+    assert dataparallel is not None
+
+
+@cuda_only
+@needs_cuda
+@needs_multiple_gpus
+def test_data_parallel_lstm_evaluates(lstm: ContextualLSTM):
+    encoding = [
+        torch.tensor([0, 1, 5, 8, 3, 1], dtype=torch.long, device=lstm.device),
+        torch.tensor([1, 4, 6, 1, 9, 7], dtype=torch.long, device=lstm.device),
+        torch.tensor([9, 0, 6, 9, 9, 0], dtype=torch.long, device=lstm.device),
+        torch.tensor([2, 3, 6, 1, 2, 4], dtype=torch.long, device=lstm.device),
+    ]
+
+    dataparallel = DataParallel(lstm)
+    result = dataparallel(encoding)
+
+    assert result is not None
+
+
+@cuda_only
+@needs_cuda
+@needs_multiple_gpus
+def test_data_parallel_returns_same_result_as_serial(lstm: ContextualLSTM, glove_embeddings: WordEmbeddings):
+    encoding = [
+        torch.tensor([0, 1, 5, 8, 3, 1], dtype=torch.long, device=lstm.device),
+        torch.tensor([1, 4, 6, 1, 9, 7], dtype=torch.long, device=lstm.device),
+        torch.tensor([9, 0, 6, 9, 9, 0], dtype=torch.long, device=lstm.device),
+        torch.tensor([2, 3, 6, 1, 2, 4], dtype=torch.long, device=lstm.device),
+    ]
+    serial = ContextualLSTM(glove_embeddings, device="cuda")
+    serial_result = serial(encoding)
+
+    parallel = DataParallel(lstm)
+    parallel_result = parallel(encoding)
+
+    assert serial_result.cpu().numpy() == pytest.approx(parallel_result.cpu().numpy())
+
+
+@needs_cuda
+@needs_multiple_gpus
+def test_train_data_parallel_lstm():
+    assert 0
+
+
+@needs_cuda
+@needs_multiple_gpus
+def test_train_distributed_lstm():
+    assert 0
+
+# TODO: Structure these properly; make one test to compare times depend on all of these
+# see pytest-dependency
+
+
+@needs_cuda
+@needs_multiple_gpus
+def test_multi_cuda_faster_than_1_cuda():
+    assert 0
+
+
+def test_distributed_faster_than_multi_cuda():
+    assert 0
