9dd417362fbafb3a0a0b55bcfbeb9ffec8ad13c6
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-15 12:07:30 -0400

    Handle possibly invalid JSON

--- sockpuppet/model/data/extract_bots.py
+++ sockpuppet/model/data/extract_bots.py
@@ -11,17 +11,21 @@ def main():
         # TODO: Give this via a command line arg
         userset = frozenset(u.casefold().strip() for u in users.readlines())
         for line in fileinput.input("-"):
-            # TODO: Generalize to multiple input streams
-            tweet = json.loads(line)
+            try:
+                # TODO: Generalize to multiple input streams
+                tweet = json.loads(line)
 
-            if "user" in tweet:
-                user = tweet["user"]
+                if "user" in tweet:
+                    user = tweet["user"]
 
-                if "screen_name" in user:
-                    screen_name = user["screen_name"]
+                    if "screen_name" in user:
+                        screen_name = user["screen_name"]
 
-                    if screen_name.casefold() in userset:
-                        print(line, end="")
+                        if screen_name.casefold() in userset:
+                            print(line, end="")
+            except json.JSONDecodeError as e:
+                print(e, file=sys.stderr)
+                print(line, end="", file=sys.stderr)
 
 
 if __name__ == "__main__":
