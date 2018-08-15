5998d6afba7f0d92bc8b382228874a1cab93a143
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-15 12:08:26 -0400

    Oops

--- tests/test_multi_gpu.py
+++ tests/test_multi_gpu.py
@@ -21,17 +21,18 @@ from tests.marks import needs_cuda, needs_cudnn, needs_multiple_gpus, cuda_only
 torch.manual_seed(0)
 
 
-@cuda_only
 @needs_cuda
+@cuda_only
 @needs_multiple_gpus
-def test_create_data_parallel_lstm(lstm: ContextualLSTM):
+def test_create_data_parallel_lstm(device, lstm: ContextualLSTM):
+    # TODO: This test is running on my laptop, but it shouldn't as I only have one GPU
     dataparallel = DataParallel(lstm)
 
     assert dataparallel is not None
 
 
-@cuda_only
 @needs_cuda
+@cuda_only
 @needs_multiple_gpus
 def test_data_parallel_lstm_evaluates(lstm: ContextualLSTM):
     encoding = [
@@ -47,17 +48,17 @@ def test_data_parallel_lstm_evaluates(lstm: ContextualLSTM):
     assert result is not None
 
 
-@cuda_only
-@needs_cuda
 @needs_multiple_gpus
-def test_data_parallel_returns_same_result_as_serial(lstm: ContextualLSTM, glove_embeddings: WordEmbeddings):
+@needs_cuda
+@cuda_only
+def test_data_parallel_returns_same_result_as_serial(lstm: ContextualLSTM, glove_embedding: WordEmbeddings):
     encoding = [
         torch.tensor([0, 1, 5, 8, 3, 1], dtype=torch.long, device=lstm.device),
         torch.tensor([1, 4, 6, 1, 9, 7], dtype=torch.long, device=lstm.device),
         torch.tensor([9, 0, 6, 9, 9, 0], dtype=torch.long, device=lstm.device),
         torch.tensor([2, 3, 6, 1, 2, 4], dtype=torch.long, device=lstm.device),
     ]
-    serial = ContextualLSTM(glove_embeddings, device="cuda")
+    serial = ContextualLSTM(glove_embedding, device="cuda")
     serial_result = serial(encoding)
 
     parallel = DataParallel(lstm)
@@ -66,12 +67,14 @@ def test_data_parallel_returns_same_result_as_serial(lstm: ContextualLSTM, glove
     assert serial_result.cpu().numpy() == pytest.approx(parallel_result.cpu().numpy())
 
 
+@pytest.mark.skip
 @needs_cuda
 @needs_multiple_gpus
 def test_train_data_parallel_lstm():
     assert 0
 
 
+@pytest.mark.skip
 @needs_cuda
 @needs_multiple_gpus
 def test_train_distributed_lstm():
@@ -81,11 +84,13 @@ def test_train_distributed_lstm():
 # see pytest-dependency
 
 
+@pytest.mark.skip
 @needs_cuda
 @needs_multiple_gpus
 def test_multi_cuda_faster_than_1_cuda():
     assert 0
 
 
+@pytest.mark.skip
 def test_distributed_faster_than_multi_cuda():
     assert 0
