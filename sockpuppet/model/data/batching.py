5448abc4d60add9945e8bc0de4b9d3947540647a
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-25 19:47:28 -0400

    Remove unused inport

--- sockpuppet/model/data/batching.py
+++ sockpuppet/model/data/batching.py
@@ -2,7 +2,7 @@ from typing import Sequence, Union, Tuple
 from collections import namedtuple
 import torch
 from torch import Tensor, LongTensor
-from torch.nn.utils.rnn import pad_sequence, pack_sequence, PackedSequence
+from torch.nn.utils.rnn import pad_sequence, pack_sequence
 
 PackedSentences = namedtuple("PackedSentences", ["packed", "lengths"])
 PaddedSequence = namedtuple("PaddedSequence", ["data", "lengths"])
