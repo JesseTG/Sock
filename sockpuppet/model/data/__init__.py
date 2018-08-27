ba3464a982e8fb50741899c200f737178e60a6ac
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-08-26 22:42:51 -0400

    Re-export PaddedSequence

--- sockpuppet/model/data/__init__.py
+++ sockpuppet/model/data/__init__.py
@@ -1,3 +1,3 @@
 from .embedding import WordEmbeddings
-from .batching import sentence_pad, sentence_label_pad
+from .batching import sentence_pad, sentence_label_pad, PaddedSequence
 from .twitter_tokenize import tokenize
