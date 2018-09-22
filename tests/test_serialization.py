63eff617b657dbc5539309621c9a3873f811bf40
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-09-22 18:10:44 -0400

    Add proper serialization tests

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,45 +1,48 @@
 import os.path
 import lzma
 import pytest
+import shutil
+
+import torch
 
 from sockpuppet.model.nn import ContextualLSTM
 from sockpuppet.model.data import WordEmbeddings
-from sockpuppet.model.nn.ContextualLSTM import save, load
+from sockpuppet.model.serial import save, load
 
 from tests.marks import *
 
 
-def test_save(tmpdir, lstm_cpu: ContextualLSTM):
-    path = os.path.join(tmpdir, "test_save.pkl")
-    save(lstm_cpu, path)
+@pytest.fixture
+def path(request, tmpdir):
+    p = os.path.join(tmpdir, f"{request.node.name}.pt")
+    yield p
+    if "keep_saved" not in request.keywords:
+        shutil.rmtree(tmpdir)
+
+
+@modes("cpu", "cuda")
+@keep_saved
+def test_save(path, lstm: ContextualLSTM):
+    save(lstm, path)
     assert os.path.exists(path)
     assert os.path.isfile(path)
     assert os.path.getsize(path) > 0
 
 
-def test_load_same_device(tmpdir, glove_embedding_cpu: WordEmbeddings, lstm_cpu: ContextualLSTM):
-    path = os.path.join(tmpdir, "test_load_same_device.pkl")
-    save(lstm_cpu, path)
+@modes("cpu", "cuda")
+def test_load_same_device(path, glove_embedding: WordEmbeddings, lstm: ContextualLSTM, device: torch.device):
+    save(lstm, path)
 
-    assert os.path.exists(path)
-    assert os.path.isfile(path)
-    assert os.path.getsize(path) > 0
-
-    loaded = load(glove_embedding_cpu, path, "cpu")
+    loaded = load(glove_embedding, path, device)
 
     assert loaded is not None
     assert isinstance(loaded, ContextualLSTM)
-    assert loaded.device.type == "cpu"
+    assert loaded.device.type == device.type
 
 
 @needs_cuda
-def test_load_different_device(tmpdir, glove_embedding_cpu: WordEmbeddings, lstm_cuda: ContextualLSTM):
-    path = os.path.join(tmpdir, "test_load_different_device.pkl")
+def test_load_different_device(path, glove_embedding_cpu: WordEmbeddings, lstm_cuda: ContextualLSTM):
     save(lstm_cuda, path)
-
-    assert os.path.exists(path)
-    assert os.path.isfile(path)
-    assert os.path.getsize(path) > 0
 
     loaded = load(glove_embedding_cpu, path, "cpu")
 
@@ -48,22 +51,33 @@ def test_load_different_device(tmpdir, glove_embedding_cpu: WordEmbeddings, lstm
     assert loaded.device.type == "cpu"
 
 
-def test_saving_doesnt_remove_embeddings(tmpdir, lstm_cpu: ContextualLSTM):
-    path = os.path.join(tmpdir, "test_saving_doesnt_remove_embeddings.pkl")
-    state = lstm_cpu.state_dict()
+@modes("cpu", "cuda")
+def test_saving_doesnt_remove_embeddings(path, lstm: ContextualLSTM):
+    state = lstm.state_dict()
 
-    save(lstm_cpu, path)
+    save(lstm, path)
 
     assert "embeddings.weight" in state
 
 
-def test_saved_model_has_same_parameters():
-    pytest.xfail()
+@modes("cpu", "cuda")
+def test_saved_model_has_same_parameters(path, glove_embedding: WordEmbeddings, lstm: ContextualLSTM, device: torch.device):
+    save(lstm, path)
+
+    loaded = load(glove_embedding, path, device)
+
+    loaded_params = [p for p in loaded.output.parameters()]
+    saved_params = [p for p in lstm.output.parameters()]
+    # Don't compare the embeddings; too expensive
+
+    for old, new in zip(loaded_params, saved_params):
+        assert old.detach().cpu().numpy() == pytest.approx(new.detach().cpu().numpy())
 
 
-def test_saved_model_doesnt_have_embeddings():
-    pytest.xfail()
+@modes("cpu", "cuda")
+def test_saved_model_doesnt_have_embeddings(path, lstm: ContextualLSTM, device: torch.device):
+    save(lstm, path)
 
+    state = torch.load(path, device)
 
-def test_metadata_is_preserved():
-    pytest.xfail()
+    assert "embeddings.weight" not in state
