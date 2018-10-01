381da012050d72144c48645734ff4a7664548779
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-01 09:55:21 -0400

    Remove unused fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -110,46 +110,6 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
         terminalreporter.line("")
 
 
-@pytest.fixture
-def app():
-    """An application for the tests."""
-    _app = create_app(TestConfig)
-    ctx = _app.test_request_context()
-    ctx.push()
-
-    yield _app
-
-    ctx.pop()
-
-
-@pytest.fixture
-def testapp(app):
-    """A Webtest app."""
-    return TestApp(app)
-
-
-@pytest.fixture
-def db(app):
-    """A database for the tests."""
-    _db.app = app
-    with app.app_context():
-        _db.create_all()
-
-    yield _db
-
-    # Explicitly close DB connection
-    _db.session.close()
-    _db.drop_all()
-
-
-@pytest.fixture
-def user(db):
-    """A user for the tests."""
-    user = UserFactory(password='myprecious')
-    db.session.commit()
-    return user
-
-
 @pytest.fixture(scope="session", params=[
     pytest.param("cpu", marks=[cpu]),
     pytest.param("cuda", marks=[cuda, needs_cuda, needs_cudnn]),
