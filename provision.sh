1be640b47c3f0e20b31ac624622a82c07120a036
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-16 14:23:55 -0400

    Improve provision script
    - Remove some packages when we don't need them

--- provision.sh
+++ provision.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
 
 apt-get update
-apt-get dist-upgrade -y
-apt-get autoremove -y
-apt-get clean -y
-apt-get autoclean -y
-pip3.7 install --no-cache-dir --compile --requirement requirements/prod.txt
+apt-get dist-upgrade --yes
+apt-get install --no-install-recommends --yes gcc libc-dev
+# Need gcc to compile some Python modules
+
+pip3.7 install -v --no-cache-dir --compile --requirement requirements/prod.txt
+# Install the Python modules
+
+apt-get purge --yes binutils gcc manpages libc-dev-bin libsqlite* *-dev
+apt-get autoremove --yes
+apt-get clean --yes
+apt-get autoclean --yes
+# Now we don't need gcc
+
+rm -rf /var/cache /var/log /var/backups
+mkdir -p /var/cache /var/log /var/backups
+# Clean the caches for good measure
