61c64da790ffb839c6bc3cfd0193b16bbc0e712e
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-16 14:35:50 -0400

    Make the health check conform to JSON-RPC 2.0

--- sock/cli/healthcheck/__main__.py
+++ sock/cli/healthcheck/__main__.py
@@ -8,6 +8,7 @@ import zmq
 from zmq import Context, Socket
 
 from sock.settings import ProdConfig
+from sock.utils import MIN_JSON_INT, MAX_JSON_INT
 
 
 def main():
@@ -16,13 +17,26 @@ def main():
     socket.connect(ProdConfig.SERVER_BIND_ADDRESS)
 
     num_tweets = random.randint(1, 30)
+    id_number = random.randint(MIN_JSON_INT, MAX_JSON_INT)
     tweets = tuple(lorem.sentence() for i in range(num_tweets))
+    request = {
+        "jsonrpc": "2.0",
+        "id": id_number,
+        "method": "guess",
+        "params": tweets
+    }
 
-    socket.send_json(tweets)
-    guesses = socket.recv_json()
+    socket.send_json(request)
+    response = socket.recv_json()  # type: dict
 
-    if len(guesses) != num_tweets:
-        raise RuntimeError(f"Expected to receive {num_tweets} guesses, got {len(guesses)}")
+    if 'jsonrpc' not in response or response['jsonrpc'] != "2.0":
+        raise ValueError("Response was not valid JSON-RPC 2.0")
+    elif id_number != response.get("id"):
+        raise ValueError(f"Expected to receive ID of {id_number}, got {response.get('id')}")
+    elif "result" not in response:
+        raise ValueError("No 'result' field found in response")
+    elif len(response['result']) != num_tweets:
+        raise RuntimeError(f"Expected to receive {num_tweets} guesses, got {len(response['result'])}")
 
     # TODO: When I eventually store a hash of the embedding vectors in the
     # model, validate the hash here too
